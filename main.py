f38e6f3508d84d19e715c125fe94bc3316e7bb80
giddeladewoye20-droid <giddeladewoye20@gmail.com>
2025-09-27 17:42:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -11,15 +11,27 @@ score=0
 msg=""
 flower=Actor("flower")
 flower.pos=100,100
+gameover=False
+
+def timer():
+    global gameover
+    gameover=True
+clock.schedule(timer,10)
 
 def draw():
     screen.clear()
-    screen.blit("background", (0,0))
-
-    bee.draw()
-    flower.draw()
+    if gameover==False:
+        screen.blit("background", (0,0))
+        bee.draw()
+        flower.draw()
+        screen.draw.text(msg,center=(250,250),fontsize=30,color="purple")
+    else:
+        screen.fill("black")
+        screen.draw.text("GAMEOVER\nYour total points are"+ str (score),center=(250,250),fontsize=60,color="yellow")
 
 def update():
+    global msg
+    global score
     if keyboard.left:
         bee.x= bee.x-2
     elif keyboard.right:
@@ -30,7 +42,10 @@ def update():
         bee.y= bee.y+2
     if bee.colliderect(flower):
         flower.pos=random.randint(0,600), random.randint(0,600)
+        score= score + 1
+        msg="You have hit the Flower!You now have " + str (score)+"points"
 
+   
 
 
 
